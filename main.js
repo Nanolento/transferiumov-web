3625df1e5d79615791db90190cd89c4c15e8c32b
Jelmer <jelmersmit@upcmail.nl>
2023-10-26 21:24:01 +0200

    Search query now generates fancy list

--- main.js
+++ main.js
@@ -1,6 +1,39 @@
+
+function jsonToStopList(j) {
+    /*
+    let listItem = document.createElement("li");
+    let listItemLink = document.createElement("a");
+    listItemLink.href = $("#inputLinkLocation").val();
+    listItemText = document.createTextNode($("#inputLinkName").val());
+    listItemLink.appendChild(listItemText);
+    listItem.appendChild(listItemLink);
+    $("#linkList").append(listItem);
+    */
+    for (let i = 0; i < j.length; i++) {
+        let stopListItem = document.createElement("div");
+        
+        let stopNameElem = document.createElement("a");
+        stopNameElem.href = "https://9292.nl";
+        let stopNameElemText = document.createTextNode(j[i].name);
+        stopNameElem.appendChild(stopNameElemText);
+        stopListItem.appendChild(stopNameElem);
+        
+        let stopDescElem = document.createElement("p");
+        $(stopDescElem).html("Type: " + j[i].loc_type + ", ID: " + j[i].stop_id.toString())
+        stopListItem.appendChild(stopDescElem);
+        $(stopListItem).addClass("searchResult");
+        $("#searchResults").append(stopListItem);
+    }
+    
+}
+
+
 $(function() {
     $("#searchBtn").click(function() {
-        sQuery = $("#query").val()
+        if ($("#searchOption").val() == "route") {
+            location.href = "https://qbuzz.nl/gd";
+        }
+        let sQuery = $("#query").val()
         $.ajax({
             url: "http://localhost:5000/search_stop",
             type: "get",
@@ -9,7 +42,7 @@ $(function() {
             },
             dataType: "json",
             success: function(response) {
-                $("#output").html(JSON.stringify(response));
+                jsonToStopList(response);
             },
             error: function (req, status, err) {
                 alert('Something went wrong', status, err);
