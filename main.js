dd223c66b7e805455d4c5d130acabc3d3a00f96a
Jelmer <jelmersmit@upcmail.nl>
2023-10-26 20:27:47 +0200

    Added simple AJAX request for searching stops
    - search button searches for stops
    - json output is dumped into textbox

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 $(function() {
-    alert("hi");
+    $("#searchBtn").click(function() {
+        sQuery = $("#query").val()
+        $.ajax({
+            url: "http://localhost:5000/search_stop",
+            type: "get",
+            data: {
+                query: sQuery
+            },
+            dataType: "json",
+            success: function(response) {
+                $("#output").html(JSON.stringify(response));
+            },
+            error: function (req, status, err) {
+                alert('Something went wrong', status, err);
+            }
+        });
+    });
 });
