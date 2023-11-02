af8af59e2aa39a0d583d0ab1ce38562601ef60ef
Jelmer <jelmersmit@upcmail.nl>
2023-11-02 20:17:04 +0100

    Rewrote trip list generating code
    - the old approach was very messy and did not sort
      correctly.
    - this new approach adds the platform code, and does
      the work synchronously.
    - the platform code does still need to be added to the
      resulting HTML elements.

--- main.js
+++ main.js
@@ -75,99 +75,85 @@ function jsonToStopList(j) {
 }
 
 
-function addStopTimeToTripList(st) {
-    let tripElem = document.createElement("div");
-    let tripTopBox = document.createElement("div");
-    let tripBottomBox = document.createElement("div");
-    let destName = document.createElement("a");
-    destName.href = "https://google.com";
-    // headsign
-    if ("stop_headsign" in st && st.stop_headsign != "") {
-        $(destName).text(st.stop_headsign);
-    } else {
-        $(destName).text(st.trip.headsign);
-    }
+function addStopTimesToTripList(stopTimes) {
+    stopTimes.forEach(st => {
+        let tripElem = document.createElement("div");
+        let tripTopBox = document.createElement("div");
+        let tripBottomBox = document.createElement("div");
+        let destName = document.createElement("a");
+        destName.href = "https://google.com";
+        // headsign
+        if ("stop_headsign" in st && st.stop_headsign != "") {
+            $(destName).text(st.stop_headsign);
+        } else {
+            $(destName).text(st.trip.headsign);
+        }
 
-    let agencyElem = document.createElement("p");
-    let shortName = document.createElement("p");
-    $(agencyElem).text(agencyNiceNames[st.route.agency]);
-    $(shortName).text(st.route.short_name);
-    // departure time
-    let depTime = document.createElement("p");
-    $(depTime).text(st.depart_time.substring(0,5));
-    
-    // distance
-    let distElem = document.createElement("p");
-    $(distElem).text((st.distance / 1000).toFixed(1) + " km");
-    
-    
-    $(destName).addClass("tl_destname");
-    $(distElem).addClass("tl_distelem");
-    $(agencyElem).addClass("tl_agency");
-    $(depTime).addClass("tl_deptime");
-    $(tripTopBox).addClass("tl_top");
-    $(tripBottomBox).addClass("tl_bottom");
-    if (st.route.fgcolor != "NULL" && st.route.bgcolor != "NULL" && st.route.type != "train") {
-        $(tripTopBox).css({
-            "color": "#" + st.route.fgcolor,
-            "background-color": "#" + st.route.bgcolor + "99"
-        });
-        $(shortName).css({
-            "background-color": "#" + st.route.bgcolor
-        });
-    } else if (st.route.type != "train") {
-        $(tripTopBox).css({
-            "background-color": "#dddddd40"
-        });
-        $(shortName).css({
-            "background-color": "#dddddd64"
-        });
-    } else {
-        $(tripTopBox).css({
-            "background-color": "#dddddd40"
-        });
-        $(destName).css({
-            "margin": "1em 1em"
-        });
-    }
-    
-    // append elems
-    $(tripTopBox).append(depTime);
-    if (st.route.type == "train") {
-        $(shortName).addClass("tl_trainname");
-        $(tripBottomBox).append(shortName);
-    } else {
-        $(shortName).addClass("tl_shortname");
-        $(tripTopBox).append(shortName);
-    }
-    $(tripTopBox).append(destName);
-    
-    $(tripBottomBox).append(distElem);
-    $(tripBottomBox).append(agencyElem);
-    
-    $(tripElem).append(tripTopBox);
-    $(tripElem).append(tripBottomBox);
-    $(tripElem).addClass("searchResult");
-    
-    // place elem at correct position.
-    if ($("#tripList").children(".searchResult").length > 0) {
-        $("#tripList").children(".searchResult").each(function() {
-            let compareStringA = $(this).children(".tl_top").children().first().first().text();
-            let compareStringB = $(depTime).text();
-            let compareIntA = parseInt(compareStringA.substring(0,2)) * 60 + parseInt(compareStringA.substring(3,5));
-            let compareIntB = parseInt(compareStringB.substring(0,2)) * 60 + parseInt(compareStringB.substring(3,5));
-            console.log(compareStringA, compareStringB, compareIntA, compareIntB);
-            if (compareIntB < compareIntA) {
-                $(tripElem).insertBefore($(this));
-            } else if (compareIntB > compareIntA) {
-                $(tripElem).insertAfter($(this));
-            }
-        });
-    } else {
+        let agencyElem = document.createElement("p");
+        let shortName = document.createElement("p");
+        $(agencyElem).text(agencyNiceNames[st.route.agency]);
+        $(shortName).text(st.route.short_name);
+        // departure time
+        let depTime = document.createElement("p");
+        $(depTime).text(st.depart_time.substring(0,5));
+        
+        // distance
+        let distElem = document.createElement("p");
+        $(distElem).text((st.distance / 1000).toFixed(1) + " km");
+        
+        
+        $(destName).addClass("tl_destname");
+        $(distElem).addClass("tl_distelem");
+        $(agencyElem).addClass("tl_agency");
+        $(depTime).addClass("tl_deptime");
+        $(tripTopBox).addClass("tl_top");
+        $(tripBottomBox).addClass("tl_bottom");
+        if (st.route.fgcolor != "NULL" && st.route.bgcolor != "NULL" && st.route.type != "train") {
+            $(tripTopBox).css({
+                "color": "#" + st.route.fgcolor,
+                "background-color": "#" + st.route.bgcolor + "99"
+            });
+            $(shortName).css({
+                "background-color": "#" + st.route.bgcolor
+            });
+        } else if (st.route.type != "train") {
+            $(tripTopBox).css({
+                "background-color": "#dddddd40"
+            });
+            $(shortName).css({
+                "background-color": "#dddddd64"
+            });
+        } else {
+            $(tripTopBox).css({
+                "background-color": "#dddddd40"
+            });
+            $(destName).css({
+                "margin": "1em 1em"
+            });
+        }
+        
+        // append elems
+        $(tripTopBox).append(depTime);
+        if (st.route.type == "train") {
+            $(shortName).addClass("tl_trainname");
+            $(tripBottomBox).append(shortName);
+        } else {
+            $(shortName).addClass("tl_shortname");
+            $(tripTopBox).append(shortName);
+        }
+        $(tripTopBox).append(destName);
+        
+        $(tripBottomBox).append(distElem);
+        $(tripBottomBox).append(agencyElem);
+        
+        $(tripElem).append(tripTopBox);
+        $(tripElem).append(tripBottomBox);
+        $(tripElem).addClass("searchResult");
         $("#tripList").append(tripElem);
-    }
+    });
     $("#loadingIcon").remove();
 }
+
 
 function populateTrips() {
     if (window.location.href.includes("stop.htm") == false) {
@@ -183,14 +169,12 @@ function populateTrips() {
         $("#tl_header").text("Helaas, er is iets misgegaan!");
         return
     }
-    var stopInfo;
     var stopInfoRequest = new Promise(function(resolve, reject) {
         $.ajax({
             url: apiDomain + "stop_info",
             type: "get",
             data: { sid: stopId },
             success: function(response) {
-                stopInfo = response;
                 resolve(response);
             },
             error: function(response) {
@@ -229,44 +213,77 @@ function populateTrips() {
             });
         });
     }).then(function(response) {
-        
+        let stops = [];
         if (response.length == 0) {
-            // get stop trips
-            // no child stops
-            $.ajax({
-                url: apiDomain + "stop_trip_times",
-                type: "get",
-                data: { sid: stopId },
-                dataType: "json",
-                success: function(response) {
-                    for (let i = 0; i < response.length; i++) {
-                        addStopTimeToTripList(response[i]);
-                    }
-                },
-                error: function(req) {
-                    alert("Helaas, er is iets misgegaan!");
-                }
+            stops.push({
+                "stopId": stopId,
+                "platformCode": undefined
             });
         } else {
             for (let i = 0; i < response.length; i++) {
-                let cStopId = response[i].stop_id;
+                stops.push({
+                    "stopId": response[i].stop_id,
+                    "platformCode": response[i].platform_code
+                });
+            }
+        }
+        let promises = [];
+        for (let i = 0; i < stops.length; i++) {
+            promises.push(new Promise(function(resolve, reject) {
                 $.ajax({
                     url: apiDomain + "stop_trip_times",
                     type: "get",
-                    data: { sid: cStopId },
+                    data: { sid: stops[i].stopId },
                     dataType: "json",
                     success: function(response) {
-                        for (let i = 0; i < response.length; i++) {
-                            addStopTimeToTripList(response[i]);
-                        }
+                        resolve(response);
                     },
                     error: function(req) {
-                        alert("Helaas, er is iets misgegaan!");
+                        reject(new Error(`Failed to load stop trip times for stop ${stops[i].stopId}`));
                     }
                 });
-                
-            }
+            }));
         }
+        Promise.allSettled(promises).then(ffPromises => {
+            let stopTimes = [];
+            for (let i = 0; i < ffPromises.length; i++) {
+                if (ffPromises[i].status == "fulfilled") {
+                    // for this stop only
+                    // clone the array as the promise value is immutable.
+                    let localStopTimes = Array.from(ffPromises[i].value);
+                    // if it has a platform code, append platform code to the stop times.
+                    if (typeof stops[i].platformCode !== "undefined") {
+                        for (let j = 0; j < localStopTimes.length; j++) {
+                            Object.assign(localStopTimes[j], {
+                                "platformCode": stops[i].platformCode
+                            });
+                        }
+                    }
+                    
+                    stopTimes = stopTimes.concat(localStopTimes);
+                } else {
+                    let warning = document.createElement("p");
+                    $(warning).text("Helaas konden we niet alle ritten vinden die bij deze halte stoppen!");
+                    $(warning).css({
+                        "background-color": "#f5948c",
+                        "color": "black",
+                        "font-weight": "bold"
+                    });
+                    $("#tripList").append(warning);
+                }
+            }
+            // now we have all stopTimes with their platform codes.
+            // now we sort them by departure time.
+            stopTimes.sort(function(a, b) {
+                let compareIntA = parseInt(a.depart_time.substring(0,2)) * 60 + parseInt(a.depart_time.substring(3,5));
+                let compareIntB = parseInt(b.depart_time.substring(0,2)) * 60 + parseInt(b.depart_time.substring(3,5));
+                return compareIntA - compareIntB;
+            });
+            // now we can create elements.
+            // we can use nearly identical code to the previous attempt.
+            console.log(stopTimes);
+            addStopTimesToTripList(stopTimes);
+        });
     });
 }
 
@@ -301,6 +318,6 @@ $(function() {
             performSearch();
         }
     });
-    $("#betaHeader").text("alpha v0.0.8");
+    $("#betaHeader").text("alpha v0.0.11");
     populateTrips();
 });
