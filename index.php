4425505b62270bbd5e2e3283fe79ce7e254ceebd
Jelmer <jelmersmit@upcmail.nl>
2023-12-02 21:56:36 +0100

    Fixed PHP routing
    - was missing htaccess file
    - index.htm was renamed to avoid possible conflicts with index.php

--- index.php
+++ index.php
@@ -2,7 +2,15 @@
 
 include 'model.php';
 
-echo "Hello World!";
-say_hello();
+if (new_route("/tov/", "get")) {
+    echo "Home page";
+}
+elseif (new_route("/tov/test", "get")) {
+    echo "testing 123";
+}
+else {
+    http_response_code(404);
+    echo "Bad page";
+}
 
 ?>
--- model.php
+++ model.php
@@ -1,7 +1,35 @@
 <?php
 
+/* enable error reporting */
+/* FOR DEVELOPMENT ONLY */
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+/**
+ * Useless function that says hello
+ * @return void
+ */
 function say_hello() {
     echo "Hello!";
+}
+
+/**
+ * Check if the route exists
+ * @param string $route_uri URI to be matched
+ * @param string $request_type Request method
+ * @return bool
+ *
+ */
+function new_route($route_uri, $request_type){
+    $route_uri_expl = array_filter(explode('/', $route_uri));
+    $current_path_expl = array_filter(explode('/',parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)));
+    if ($route_uri_expl == $current_path_expl && $_SERVER['REQUEST_METHOD'] == strtoupper($request_type)) {
+        return True;
+    } else {
+        return False;
+    }
 }
 
 ?>
