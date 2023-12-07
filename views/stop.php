363e15b4b50e7830e0758d027007900fd1d61016
Jelmer <jelmersmit@upcmail.nl>
2023-12-07 18:46:26 +0100

    Removed no longer needed functions from main.js
    - modified style.css to bring back controls for trip list
    - removed reload button in stop view and added earlier + later btns

--- views/stop.php
+++ views/stop.php
@@ -6,7 +6,10 @@ include __DIR__ . "/../templates/header.php";
 <h2 class="tl_header" id="tl_head"><?= $stop_info['name'] ?></h2>
 
 <div id="tl_controls">
-    <p><button id="reloadBtn">Herladen</button></p>
+    <p>
+        <button id="earlierTripBtn">&lt; Eerder</button>
+        <button id="laterTripBtn">Later &gt;</button>
+    </p>
     <p><span id="tl_filter_header">Filters:</span>
         <input type="checkbox" id="placeholderFilter" name="plfilter">
         <label for="plfilter">Voorbeeldfilter</label>
