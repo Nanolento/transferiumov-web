e8a015d3f0d059f9e57f76b5589245ee060c8735
Jelmer <jelmersmit@upcmail.nl>
2024-01-25 23:13:11 +0100

    Updated copyright year

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,4 @@
-<p id="disclaimer">&copy; Copyright 2022-2023 Jelmer Smit<br>TransferiumOV is niet geassocieerd met de openbare vervoerders in
+<p id="disclaimer">&copy; Copyright 2022-2024 Jelmer Smit<br>TransferiumOV is niet geassocieerd met de openbare vervoerders in
     de Benelux.<br>TransferiumOV stelt slechts een website beschikbaar waarop de dienstregelingen
     van de Nederlandse openbaar vervoerders te zien zijn.<br>TransferiumOV maakt gebruik van GTFS data
     van, maar is niet geassocieerd met OVapi B.V.</p>
